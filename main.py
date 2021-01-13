61877a5e9fccaf0ecb785ef7b7e313e32d076d91
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-13 16:21:30 -0600

    made the shape of the snake

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from turtle import Turtle, Screen 
 
+#So far im doing it by myself
+python = Turtle()
+python.color("white")
+python.shape("square")
+#Probably going to use this
+#xcor()
+#ycor() 
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
