2b7055712bd7da16dc803742b2113ed41b95a77e
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-13 16:23:02 -0600

    made notes

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from turtle import Turtle, Screen
 python = Turtle()
 python.color("white")
 python.shape("square")
+python.pencolor("white")
+
+#might have to use forloop to add more squares 
+# or a while loop with an if statement 
+# so it only adds squares when the snake eats
 #Probably going to use this
 #xcor()
 #ycor() 
