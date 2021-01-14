6df31606d2853c244aa08971f40d579c13d1ee52
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-13 19:45:35 -0600

    making a forloop

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from turtle import Turtle, Screen 
 
-#So far im doing it by myself
-python = Turtle()
-python.color("white")
-python.shape("square")
-python.pencolor("white")
-python.speed(6)
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Python Game")
+screen.bgcolor("black")
+screen.listen()
 
+#So far im doing it by myself
+box = []
+for boxes in range (0, 4):
+    python = Turtle()
+    python.color("white")
+    python.shape("square")
+    python.pencolor("white")
+    python.speed(6)
+    python.goto(0, 0)
+    box.append(boxes)
 
 def move_forward(): 
     python.setheading(0)
     python.forward(30)
 
-python.onkey(move_forward, "w")
+# python.onkey(move_forward, "w")
 #might have to use forloop to add more squares 
 # or a while loop with an if statement 
 # so it only adds squares when the snake eats
@@ -21,8 +30,9 @@ python.onkey(move_forward, "w")
 #ycor() 
 
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.title("Python Game")
-screen.bgcolor("black")
+# screen = Screen()
+# screen.setup(width=600, height=600)
+# screen.title("Python Game")
+# screen.bgcolor("black")
+# screen.listen()
 screen.exitonclick()
