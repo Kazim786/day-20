0ffcc0bcaa3671c5c977fb556153e9e1ab2370db
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-18 21:20:44 -0600

    edited scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -4,9 +4,10 @@ class Scoreboard(Turtle): #Inheriting from Turtle
     def __init__(self): #Attributes go here and method calls
         super().__init__()
         self.score = 0
+        self.color("white")
+        self.hideturtle()
         self.write(f"Score: {self.score}", True, align= "center", font= ("Arial", 24, "normal"))
         # self.write("Score = 0", False, align= "center")
-
         self.goto(0, 250)
-        self.pencolor("white")
+        #Did all this by myself
         
