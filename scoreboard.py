18310b0437f07ec1e0cb5259a51a85515bdfa3e4
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-18 21:23:25 -0600

    rearranged lines

--- scoreboard.py
+++ scoreboard.py
@@ -6,8 +6,9 @@ class Scoreboard(Turtle): #Inheriting from Turtle
         self.score = 0
         self.color("white")
         self.hideturtle()
+        self.goto(0, 250)
         self.write(f"Score: {self.score}", True, align= "center", font= ("Arial", 24, "normal"))
         # self.write("Score = 0", False, align= "center")
-        self.goto(0, 250)
+        
         #Did all this by myself
         
