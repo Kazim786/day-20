8494d4ee86234be404a62ec1f47c3d2a0fa986a9
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-18 21:18:24 -0600

    fixed up the self.write

--- scoreboard.py
+++ scoreboard.py
@@ -4,7 +4,7 @@ class Scoreboard(Turtle): #Inheriting from Turtle
     def __init__(self): #Attributes go here and method calls
         super().__init__()
         self.score = 0
-        self.write(f"Score = {self.score}", True, align= "center")
+        self.write(f"Score: {self.score}", True, align= "center", font= ("Arial", 24, "normal"))
         # self.write("Score = 0", False, align= "center")
 
         self.goto(0, 250)
